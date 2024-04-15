09c440eb95e1a6a45ee1906c273884361c65acd4
bulboni <163756439+bulboni@users.noreply.github.com>
2024-04-14 20:06:45 -0400

    Update rd.sh

--- rd.sh
+++ rd.sh
@@ -3,6 +3,6 @@ DEBIAN_FRONTEND=noninteractive \
 apt install --assume-yes xfce4 desktop-base dbus-x11 xscreensaver && \
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb && \
 dpkg -i chrome-remote-desktop_current_amd64.deb && \
-apt-get install -f && \
+apt install -f && \
 bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session' && \
 systemctl disable lightdm.service
