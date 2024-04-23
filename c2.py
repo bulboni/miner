3ebe097fc02c9f806bce637d0cae7c4c9a50553d
bulboni <163756439+bulboni@users.noreply.github.com>
2024-04-23 04:33:19 -0400

    Update c2.py

--- c2.py
+++ c2.py
@@ -5,7 +5,8 @@ import random
 # Daftar koordinat dan jumlah klik
 click_coordinates = [
     (183, 51, 1),  # Klik 1x di koordinat (183, 51)
-    (82, 493, 2),  # Klik 2x di koordinat (82, 493)
+    (82, 493, 1),  # Klik 2x di koordinat (82, 493)
+    (82, 494, 1),  # Klik 2x di koordinat (82, 493)
     # Tambahkan koordinat dan jumlah klik lainnya di sini
 ]
 
@@ -27,9 +28,13 @@ while current_time - start_time < total_duration:
     # Memainkan kembali rekaman gerakan mouse
     for coordinate in click_coordinates:
         x, y, clicks = coordinate
-        for _ in range(clicks):
-            pyautogui.click(x, y)  # Melakukan klik sesuai jumlah yang ditentukan
-            time.sleep(0.1)  # Jeda singkat antara klik
+        if (x, y) == (82, 493):
+            pyautogui.click(x, y)  # Melakukan klik di koordinat (82, 493)
+            time.sleep(10)  # Menunggu 10 detik setelah klik di koordinat (82, 493)
+        else:
+            for _ in range(clicks):
+                pyautogui.click(x, y)  # Melakukan klik sesuai jumlah yang ditentukan
+                time.sleep(0.1)  # Jeda singkat antara klik
     
     # Menunggu sebentar sebelum mengulang langkah berikutnya
     time.sleep(interval)
