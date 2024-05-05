e45323867c65138df7c5ab49abed75b2e9d30094
bulboni <163756439+bulboni@users.noreply.github.com>
2024-05-04 20:53:58 -0400

    Update db.py

--- db.py
+++ db.py
@@ -23,23 +23,23 @@ while current_time - start_time < total_duration:
     if click_counter < 100:
         interval = 60  # Jeda waktu setiap 60 detik untuk 100 klik pertama
     else:
-        interval = 300  # Jeda waktu setiap 5 menit setelah 100 klik pertama
+        interval = 120  # Jeda waktu setiap 5 menit setelah 100 klik pertama
 
     random_delay = random.uniform(2, 6)  # Jeda waktu acak antara 2 hingga 6 detik
     time.sleep(random_delay)
     
     # Memainkan kembali rekaman gerakan mouse
     for action in recorded_mouse_actions:
-        if action[0] == 631 and action[1] == 378:  # Jika mouse berada di koordinat (631, 378)
+        if action[0] == 374 and action[1] == 240:  # Jika mouse berada di koordinat (631, 378)
             pyautogui.moveTo(action[0], action[1], duration=action[2])
             pyautogui.doubleClick()  # Melakukan double klik
             time.sleep(60)  # Tunggu 1 menit
             pyautogui.hotkey('ctrl', 'c')  # Tekan tombol Ctrl + C
             pyautogui.press('enter')  # Tekan tombol Enter
-        elif action[0] == 700 and action[1] == 751:  # Jika mouse berada di koordinat (700, 751)
+        elif action[0] == 486 and action[1] == 245:  # Jika mouse berada di koordinat (700, 751)
             pyautogui.moveTo(action[0], action[1], duration=action[2])
             pyautogui.doubleClick()  # Melakukan double klik
-            pyautogui.moveTo(631, 378, duration=0.5)  # Pindahkan mouse ke koordinat (631, 378)
+            pyautogui.moveTo(374, 240, duration=0.5)  # Pindahkan mouse ke koordinat (631, 378)
         else:
             pyautogui.moveTo(action[0], action[1], duration=action[2])
             pyautogui.doubleClick()  # Melakukan double klik pada setiap langkah
